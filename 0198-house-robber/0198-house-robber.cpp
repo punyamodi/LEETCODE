511e643c6be65b143443a0b15967284a8d3436fa
Punya Modi <68418104+punyamodi@users.noreply.github.com>
2024-10-08 23:47:24 +0530

    Time: 4 ms (32.14%), Space: 9.2 MB (99.21%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size(), prev1=0,prev2=0;
+
+        for(int i=0;i<n;i++){
+            int temp=prev1;        
+            prev1=max(prev2+nums[i],prev1);
+            prev2=temp;
+        }
+        return prev1;
+    }
+};
