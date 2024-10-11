51226e327e9e4917ff07770fad601aa0ee2eee4f
Punya Modi <68418104+punyamodi@users.noreply.github.com>
2024-10-12 01:31:31 +0530

    Time: 0 ms (100%), Space: 15.9 MB (74.63%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        prices.push_back(0);
+        int curr_min=INT_MAX;
+        int curr_max=INT_MIN;
+        int profit = 0;
+        for(int i=0;i<prices.size();i++){
+            if(prices[i]<curr_max){
+                profit += curr_max - curr_min;
+                curr_min = INT_MAX;
+                curr_max=INT_MIN;
+            }
+            curr_min = min(curr_min,prices[i]);
+            curr_max = max(curr_max,prices[i]);
+        }
+        return profit;
+    }
+};
+
+/*class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
         int n=prices.size();
         int buy=0,profit=0;
         bool holding=false;
@@ -19,4 +39,4 @@ public:
         }
         return profit;
     }
-};
+};*/
